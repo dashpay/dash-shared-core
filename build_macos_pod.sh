08dcf288f72d81ca26a5f0d54ad66569cc8d3400
pankcuf <rastacake@gmail.com>
2023-04-19 15:33:39 +0400

    [0.3.0: RC]

--- build_macos_pod.sh
+++ build_macos_pod.sh
@@ -15,7 +15,10 @@ rustup target add aarch64-apple-darwin
 
 cargo lipo --release
 cargo build --target=x86_64-apple-darwin --release
+# Do we really need this?
+#cargo build --target=aarch64-apple-darwin --release
 lipo -create target/x86_64-apple-darwin/release/libdash_shared_core.a -output target/universal/release/libdash_shared_core_macos.a
+#lipo -create target/aarch64-apple-darwin/release/libdash_shared_core.a -output target/universal/release/libdash_shared_core_macos.a
 
 mkdir -p DashSharedCore/lib/macos
 mkdir -p DashSharedCore/include
