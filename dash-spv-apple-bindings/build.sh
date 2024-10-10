53bd7b219986c0d79f31fd67bb368e2ab00e5a38
pankcuf <rastacake@gmail.com>
2024-10-10 21:30:36 +0700

    fix: adapt build script

--- dash-spv-apple-bindings/build.sh
+++ dash-spv-apple-bindings/build.sh
@@ -6,10 +6,12 @@ echo "Building Dash Shared Core..."
 pwd
 
 compare_version() {
-    if [[ $1 == $2 ]]; then
+     # shellcheck disable=SC2053
+   if [[ $1 == $2 ]]; then
         return 1
     fi
     local IFS=.
+    # shellcheck disable=SC2206
     local i a=(${1%%[^0-9.]*}) b=(${2%%[^0-9.]*})
     local arem=${1#${1%%[^0-9.]*}} brem=${2#${2%%[^0-9.]*}}
     for ((i=0; i<${#a[@]} || i<${#b[@]}; i++)); do
@@ -29,6 +31,9 @@ compare_version() {
 #REQUIRED_VERSION=1.66.0
 REQUIRED_VERSION=1.80.1
 CURRENT_VERSION=$(rustc -V | awk '{sub(/-.*/,"");print $2}')
+FRAMEWORK=DashSharedCore
+HEADER=dash_spv_apple_bindings
+
 echo "rustc -V: current ${CURRENT_VERSION} vs. required ${REQUIRED_VERSION}"
 if compare_version "${REQUIRED_VERSION}" "${CURRENT_VERSION}"; then
   echo "ERROR: rustc version ${CURRENT_VERSION} not supported, please upgrade to at least ${REQUIRED_VERSION}"
@@ -36,57 +41,68 @@ if compare_version "${REQUIRED_VERSION}" "${CURRENT_VERSION}"; then
 fi
 
 cargo install cargo-lipo
+for target in "x86_64-apple-darwin" "aarch64-apple-darwin" "x86_64-apple-ios" "aarch64-apple-ios" "aarch64-apple-ios-sim"; do
+    if ! rustup target list | grep -q "${target} (installed)"; then
+        rustup target add "$target"
+    fi
+done
 
-rm -rf DashSharedCore/include
-rm -rf DashSharedCore/lib/macos
-rm -rf DashSharedCore/framework
-rm -rf DashSharedCore/lib/ios
-rm -rf DashSharedCore/lib/ios-simulator
 
-rustup target add x86_64-apple-darwin
-rustup target add aarch64-apple-darwin
-rustup target add x86_64-apple-ios
-rustup target add aarch64-apple-ios
-rustup target add aarch64-apple-ios-sim
-
+rm -rf target/{framework,include,lib}
 cargo lipo --release
-cargo build --target=x86_64-apple-darwin --release
-cargo build --target=aarch64-apple-darwin --release
-cargo build --target=x86_64-apple-ios --release
-cargo build --target=aarch64-apple-ios --release
-cargo build --target=aarch64-apple-ios-sim --release
+build_targets=(
+    "x86_64-apple-ios"
+    "aarch64-apple-ios"
+    "aarch64-apple-ios-sim"
+    "x86_64-apple-darwin"
+    "aarch64-apple-darwin"
 
-#cargo expand > expanded.rs
-#cbindgen --config cbindgen.toml --output ../target/dash_shared_core.h
-#cbindgen --config cbindgen.toml -o ../target/dash_shared_core.h expanded.rs
+)
+for target in "${build_targets[@]}"; do
+    if [ ! -f "../../target/$target/release/lib${HEADER}.a" ]; then
+        cargo build --target="$target" --release &
+    fi
+done
+wait
+mkdir -p target/{framework,include,lib/{ios,ios-simulator,macos}}
 
-set -e
-#cargo build --release --verbose
-cargo expand | sed '/#!/d' > target/expanded.rs
-sed -n '/#\[prelude_import\]/,$p' target/expanded.rs > target/expanded_reduced.rs
-cbindgen --config cbindgen.toml -o ../target/dash_shared_core.h target/expanded_reduced.rs
+lipo -create ../target/x86_64-apple-darwin/release/lib${HEADER}.a \
+  ../target/aarch64-apple-darwin/release/lib${HEADER}.a \
+  -output target/lib/macos/lib${HEADER}_macos.a &
+cp -r -p target/include/${HEADER}.h target/framework/include
+cp -r -p ../target/aarch64-apple-ios/release/lib${HEADER}.a target/lib/ios/lib${HEADER}_ios.a &
+lipo -create ../target/x86_64-apple-ios/release/lib${HEADER}.a  \
+  ../target/aarch64-apple-ios-sim/release/lib${HEADER}.a \
+  -output target/lib/ios-simulator/lib${HEADER}_ios.a &
+wait
+wait
 
-
-mkdir -p DashSharedCore/framework
-mkdir -p DashSharedCore/include
-mkdir -p DashSharedCore/lib/ios
-mkdir -p DashSharedCore/lib/ios-simulator
-mkdir -p DashSharedCore/lib/macos
-
-lipo -create ../target/x86_64-apple-darwin/release/libdash_spv_apple_bindings.a \
-  ../target/aarch64-apple-darwin/release/libdash_spv_apple_bindings.a \
-  -output DashSharedCore/lib/macos/libdash_shared_core_macos.a
-
-cp -r -p ../target/dash_shared_core.h DashSharedCore/include
-cp -r -p ../target/aarch64-apple-ios/release/libdash_spv_apple_bindings.a DashSharedCore/lib/ios/libdash_shared_core_ios.a
-
-lipo -create ../target/x86_64-apple-ios/release/libdash_spv_apple_bindings.a \
-  ../target/aarch64-apple-ios-sim/release/libdash_spv_apple_bindings.a \
-  -output DashSharedCore/lib/ios-simulator/libdash_shared_core_ios.a
+if which clang-format >/dev/null; then
+  find "target/include" -name 'objc_wrapper.h' -print0 | xargs -0 clang-format -i -style=file
+else
+    echo "warning: clang-format not installed, install it by running $(brew install clang-format)"
+fi
 
 xcodebuild -create-xcframework \
-	-library DashSharedCore/lib/ios/libdash_shared_core_ios.a -headers DashSharedCore/include \
-	-library DashSharedCore/lib/ios-simulator/libdash_shared_core_ios.a -headers DashSharedCore/include \
-	-output DashSharedCore/framework/DashSharedCore.xcframework
+	-library target/lib/ios/lib${HEADER}_ios.a -headers target/include \
+	-library target/lib/ios-simulator/lib${HEADER}_ios.a -headers target/include \
+	-output target/framework/${FRAMEWORK}.xcframework
+
+
+#lipo -create ../target/x86_64-apple-darwin/release/libdash_spv_apple_bindings.a \
+#  ../target/aarch64-apple-darwin/release/libdash_spv_apple_bindings.a \
+#  -output DashSharedCore/lib/macos/libdash_shared_core_macos.a
+#
+#cp -r -p ../target/dash_shared_core.h DashSharedCore/include
+#cp -r -p ../target/aarch64-apple-ios/release/libdash_spv_apple_bindings.a DashSharedCore/lib/ios/libdash_shared_core_ios.a
+#
+#lipo -create ../target/x86_64-apple-ios/release/libdash_spv_apple_bindings.a \
+#  ../target/aarch64-apple-ios-sim/release/libdash_spv_apple_bindings.a \
+#  -output DashSharedCore/lib/ios-simulator/libdash_shared_core_ios.a
+#
+#xcodebuild -create-xcframework \
+#	-library DashSharedCore/lib/ios/libdash_shared_core_ios.a -headers DashSharedCore/include \
+#	-library DashSharedCore/lib/ios-simulator/libdash_shared_core_ios.a -headers DashSharedCore/include \
+#	-output DashSharedCore/framework/DashSharedCore.xcframework
 
 echo "Done building Dash Shared Core"
