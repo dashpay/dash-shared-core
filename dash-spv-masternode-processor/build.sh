2943625676c3e42035d82f278245e52e76396965
pankcuf <rastacake@gmail.com>
2024-10-20 13:39:23 +0700

    chore: expose vtables for trait implementors

--- dash-spv-masternode-processor/build.sh
+++
@@ -1,5 +0,0 @@
-set -e
-cargo build --release --verbose
-cargo expand | sed '/#!/d' > target/expanded.rs
-sed -n '/#\[prelude_import\]/,$p' target/expanded.rs > target/expanded_reduced.rs
-cbindgen --config cbindgen.toml -o target/example.h target/expanded_reduced.rs
